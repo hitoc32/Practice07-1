759650fda048b619d41d85b30998445e3f1dcc8a
hitoc32 <football32.cs@gmail.com>
2023-08-03 23:25:41 +0900

    バリデーションの追加

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -12,15 +12,17 @@
                 @csrf
                 <div class ="title">
                     <h2>Title</h2>
-                    <input type="text" name="post[title]" placeholder="タイトル"/>
+                    <input type="text" name="post[title]" placeholder="タイトル" value="{{ old('post.title') }}"/>
+                    <p class='title__error' style="color:red">{{ $errors->first('post.title') }}</p>
                 </div>
                 <div class="body">
                     <h2>Body</h2>
-                    <textarea name="post[body]" placeholder="こちらに記入してください"></textarea>
+                    <textarea name="post[body]" placeholder="こちらに記入してください">{{ old('post.body') }}</textarea>
+                    <p class='body__error' style="color:red">{{ $errors->first('post.body') }}</p>
                 </div>
-                <input type="submit" value="store"/>
+                <input type="submit" value="保存"/>
             </form>
-            <div class='footer'>
+            <div class='back'>
                 <!--ブログ投稿一覧画面に戻るためのリンクを用意する-->
                 <a href="/">ブログ投稿一覧に戻る</a>
             </div>
